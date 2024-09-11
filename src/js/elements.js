5b0937bc7dd5c6df676217cbd02e07d63e854e58
Carlos Armando Moreira <carlosmoreira.bji@gmail.com>
2024-09-11 19:22:16 -0300

    feat: modularizing buttons in elements.js file

---
+++ src/js/elements.js
@@ -0,0 +1,19 @@
+const buttonPlay = document.querySelector('.play')
+const buttonPause = document.querySelector('.pause')
+const buttonStop = document.querySelector('.stop')
+const buttonStopwatch = document.querySelector('.stopwatch')
+const buttonSoundOn = document.querySelector('.sound-on')
+const buttonSoundOff = document.querySelector('.sound-off')
+const displayMinutes = document.querySelector('.minutes')
+const displaySeconds = document.querySelector('.seconds')
+
+export {
+    buttonPlay,
+    buttonPause,
+    buttonStop,
+    buttonStopwatch,
+    buttonSoundOn,
+    buttonSoundOff,
+    displayMinutes,
+    displaySeconds
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,15 @@
 import { Controls } from "./controls.js"
 import { Timer } from "./timer.js"
-
-const buttonPlay = document.querySelector('.play')
-const buttonPause = document.querySelector('.pause')
-const buttonStop = document.querySelector('.stop')
-const buttonStopwatch = document.querySelector('.stopwatch')
-const buttonSoundOn = document.querySelector('.sound-on')
-const buttonSoundOff = document.querySelector('.sound-off')
-const displayMinutes = document.querySelector('.minutes')
-const displaySeconds = document.querySelector('.seconds')
+import {
+    buttonPause,
+    buttonPlay,
+    buttonStop,
+    buttonStopwatch,
+    buttonSoundOn,
+    buttonSoundOff,
+    displayMinutes,
+    displaySeconds
+} from "./elements.js"
 
 const controls = Controls({
     buttonPause,
