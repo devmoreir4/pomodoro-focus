982e907f2e9678529528d1a42b930afbdefe1195
Carlos Armando Moreira <carlosmoreira.bji@gmail.com>
2024-09-11 00:05:32 -0300

    updating script.js

--- src/script.js
+++ src/script.js
@@ -5,11 +5,51 @@ const buttonStopwatch = document.querySelector('.stopwatch')
 const buttonSoundOn = document.querySelector('.sound-on')
 const buttonSoundOff = document.querySelector('.sound-off')
 
+const displayMinutes = document.querySelector('.minutes')
+const displaySeconds = document.querySelector('.seconds')
+let minutes, seconds
+
+function resetControls() {
+    buttonPlay.classList.remove('hide')
+    buttonPause.classList.add('hide')
+    buttonStopwatch.classList.remove('hide')
+    buttonStop.classList.add('hide')
+}
+
+function updateTimerDisplay(minutes, seconds) {
+    displayMinutes.textContent = String(minutes).padStart(2, "0")
+    displaySeconds.textContent = String(seconds).padStart(2, "0")
+}
+
+function countdown() {
+    setTimeout(function() {
+        let seconds = Number(displaySeconds.textContent)
+        let minutes = Number(displayMinutes.textContent)
+
+        updateTimerDisplay(minutes, 0)
+
+        if (minutes <= 0) {
+            resetControls()
+            return
+        }
+
+        if (seconds <= 0) {
+            seconds = 2
+            --minutes
+        }
+
+        updateTimerDisplay(minutes, String(seconds - 1))
+
+        countdown()
+    }, 1000)
+}
+
 buttonPlay.addEventListener('click', function() {
     buttonPlay.classList.add('hide')
     buttonPause.classList.remove('hide')
     buttonStopwatch.classList.add('hide')
     buttonStop.classList.remove('hide')
+    countdown()
 })
 
 buttonPause.addEventListener('click', function() {
@@ -18,10 +58,7 @@ buttonPause.addEventListener('click', function() {
 })
 
 buttonStop.addEventListener('click', function() {
-    buttonPlay.classList.remove('hide')
-    buttonPause.classList.add('hide')
-    buttonStopwatch.classList.remove('hide')
-    buttonStop.classList.add('hide')
+    resetControls()
 })
 
 buttonSoundOff.addEventListener('click', function() {
@@ -32,4 +69,9 @@ buttonSoundOff.addEventListener('click', function() {
 buttonSoundOn.addEventListener('click', function() {
     buttonSoundOn.classList.add('hide')
     buttonSoundOff.classList.remove('hide')
+})
+
+buttonStopwatch.addEventListener('click', function() {
+    minutes = prompt('minutes?')
+    updateTimerDisplay(minutes, 0)
 })
