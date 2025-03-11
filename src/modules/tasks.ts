41bab8f169509b8b62808c5fcb451acd3df072ea
Carlos Armando Moreira <carlosmoreira.bji@gmail.com>
2025-03-10 21:52:06 -0300

    feat: add checked task and add media queries

--- src/modules/tasks.ts
+++ src/modules/tasks.ts
@@ -1,6 +1,7 @@
 export interface Task {
   id: number;
   text: string;
+  completed: boolean;
 }
 
 export interface TasksManagerProps {
@@ -14,6 +15,7 @@ export function TasksManager({ taskList }: TasksManagerProps) {
     const task: Task = {
       id: Date.now(),
       text: taskText,
+      completed: false,
     };
     tasks.push(task);
     render();
@@ -24,11 +26,44 @@ export function TasksManager({ taskList }: TasksManagerProps) {
     render();
   }
 
+  function toggleTaskCompletion(id: number) {
+    tasks = tasks.map((task) =>
+      task.id === id ? { ...task, completed: !task.completed } : task
+    );
+    render();
+  }
+
   function render() {
     taskList.innerHTML = "";
     tasks.forEach((task) => {
       const li = document.createElement("li");
-      li.textContent = task.text;
+
+      const taskContent = document.createElement("div");
+      taskContent.classList.add("task-content");
+
+      const label = document.createElement("label");
+      label.classList.add("custom-checkbox");
+
+      const checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.checked = task.completed;
+      checkbox.addEventListener("change", () => {
+        toggleTaskCompletion(task.id);
+      });
+
+      const checkboxSpan = document.createElement("span");
+
+      label.appendChild(checkbox);
+      label.appendChild(checkboxSpan);
+
+      const taskTextSpan = document.createElement("span");
+      taskTextSpan.textContent = task.text;
+      if (task.completed) {
+        taskTextSpan.classList.add("completed");
+      }
+
+      taskContent.appendChild(label);
+      taskContent.appendChild(taskTextSpan);
 
       const removeBtn = document.createElement("button");
       removeBtn.textContent = "X";
@@ -36,6 +71,7 @@ export function TasksManager({ taskList }: TasksManagerProps) {
         removeTask(task.id);
       });
 
+      li.appendChild(taskContent);
       li.appendChild(removeBtn);
       taskList.appendChild(li);
     });
